1ea88f0105eb43057d93eaf37c623e82c88a8f14
devsingla99 <43935029+devsingla99@users.noreply.github.com>
2020-11-13 20:40:44 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   description = 'TOPSIS PROGRAM',   # Give a short description about your library
   author = 'DEVANSHU SINGLA',                   # Type in your name
   author_email = 'devanshusingla99@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
+  url = 'https://github.com/devsingla99/TOPSIS',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/devsingla99/TOPSIS/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
